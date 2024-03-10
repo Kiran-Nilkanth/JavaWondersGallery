dce8839d09f1018e9970f8ca9fe68d30048a9fc3
Kiran Tukaram Nilkanth <160914992+Kiran-Nilkanth@users.noreply.github.com>
2024-03-10 19:08:19 +0530

    Add files via upload

---
+++ designpatterns/src/com/jspiders/designpatterns/creational/MasalaTea.java
@@ -0,0 +1,12 @@
+package com.jspiders.designpatterns.creational;
+
+public class MasalaTea implements Beverage {
+
+	@Override
+	public void order() {
+		System.out.println("MasalaTea is ordered");
+		
+	}
+	
+
+}

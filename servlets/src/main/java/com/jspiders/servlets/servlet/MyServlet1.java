2f8fc39f7d67c6f4fd1fdd795019b1d312211d97
Kiran Tukaram Nilkanth <160914992+Kiran-Nilkanth@users.noreply.github.com>
2024-03-10 19:18:27 +0530

    Add files via upload

---
+++ servlets/src/main/java/com/jspiders/servlets/servlet/MyServlet1.java
@@ -0,0 +1,24 @@
+package com.jspiders.servlets.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class MyServlet1 extends GenericServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.setContentType("html/text");
+		PrintWriter writer=res.getWriter();
+		writer.println("<h1>Hello World!!</h1>");
+		
+	}
+
+}
